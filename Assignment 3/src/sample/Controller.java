dcc6f2f2b3599f9bb8b0f11cad2dfe914394e586
dcervantes1020 <54913911+dcervantes1020@users.noreply.github.com>
2020-04-13 19:54:51 -0500

    Add files via upload

--- Assignment 3/src/sample/Controller.java
+++ Assignment 3/src/sample/Controller.java
@@ -45,17 +45,40 @@ public class Controller implements Initializable {
     private ComboBox itemSelectionCB;
 
     private ObservableList itemsList;
+    private ObservableList usersList;
+
     @FXML
     private Button itemCheckoutBTN;
     @FXML
     private Button userViewBTN;
+    @FXML
+    private Button createUserTableBTN;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle){
         final String DB_URL = "jdbc:derby://localhost:1527/LibraryDB;";
         itemTypeCB.getItems().addAll("Book","Media");
+        userTypeCB.getItems().addAll("Undergraduate","Graduate","Faculty","Staff","Alumni");
         itemsList = FXCollections.observableArrayList();
+        usersList = FXCollections.observableArrayList();
         itemsListView.setItems(itemsList);
+        usersListView.setItems(usersList);
+
+        try{
+            ResultSet resultSetMain=null;
+            Connection connection = DriverManager.getConnection(DB_URL);
+            PreparedStatement preparedStatement= connection.prepareStatement("SELECT Item_Title FROM items");
+            resultSetMain=preparedStatement.executeQuery();
+            while (resultSetMain.next()){
+                String title_item = resultSetMain.getString("Item_Title");
+                itemSelectionCB.getItems().add(title_item);
+            }
+        }catch (Exception exception)
+        {
+            var message = exception.getMessage();
+            System.out.println(message);
+        }
+
 
         createItemTableBTN.setOnAction(ActionEvent->{
             try{
@@ -95,7 +118,7 @@ public class Controller implements Initializable {
                 stmt.setString(5, callNo);
                 stmt.setInt(6, quantity);
                 stmt.executeUpdate();
-                System.out.println("record inserted into table");
+                System.out.println("record inserted into items table");
             }catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -105,6 +128,21 @@ public class Controller implements Initializable {
             authorTF.clear();
             callNoTF.clear();
             quantityTF.clear();
+            itemSelectionCB.getItems().clear();
+            try{
+                ResultSet resultSetMain=null;
+                Connection connection = DriverManager.getConnection(DB_URL);
+                PreparedStatement preparedStatement= connection.prepareStatement("SELECT Item_Title FROM items");
+                resultSetMain=preparedStatement.executeQuery();
+                while (resultSetMain.next()){
+                    String title_item = resultSetMain.getString("Item_Title");
+                    itemSelectionCB.getItems().add(title_item);
+                }
+            }catch (Exception exception)
+            {
+                var message = exception.getMessage();
+                System.out.println(message);
+            }
         });
 
         itemsViewBTN.setOnAction(ActionEvent->{
@@ -121,7 +159,69 @@ public class Controller implements Initializable {
                     String author = resultSet.getString("Author");
                     String callNo = resultSet.getString("Call_No");
                     int quantity = Integer.parseInt(resultSet.getString("Quantity"));
-                    itemsList.addAll(isbn + "\t" + itemType + "\t" + itemTitle + "\t" + author + "\t" + callNo + "\t" + quantity);
+                    itemsList.addAll(isbn + "     " + itemType + "     " + itemTitle + "     " + author + "     " + callNo + "     " + quantity);
+                }
+                resultSet.close();
+                conn.close();
+            }catch (Exception ex){
+                var msg = ex.getMessage();
+                System.out.println(msg);
+            }
+        });
+
+        createUserTableBTN.setOnAction(ActionEvent->{
+            try{
+                Connection conn = DriverManager.getConnection(DB_URL);
+                Statement stmt = conn.createStatement();
+                stmt.execute("CREATE TABLE users (" +
+                        "User_Type VARCHAR(50), " +
+                        "User_Name VARCHAR(50), " +
+                        "Item VARCHAR(50) )");
+                stmt.close();
+                System.out.println("Success!..Table created");
+                conn.close();
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
+
+        itemCheckoutBTN.setOnAction(ActionEvent->{
+            try {
+                Connection conn = DriverManager.getConnection(DB_URL);
+                PreparedStatement stmt = null;
+                String userType = (String) userTypeCB.getValue();
+                String userName = userNameTF.getText();
+                String item = (String) itemSelectionCB.getValue();
+                stmt = conn.prepareStatement("INSERT INTO users ( User_Type, User_Name, Item ) VALUES (?,?,?)");
+                stmt.setString(1, userType);
+                stmt.setString(2, userName);
+                stmt.setString(3, item);
+                stmt.executeUpdate();
+
+                System.out.println("Record inserted into users table");
+            }catch (Exception exception)
+            {
+                var message = exception.getMessage();
+                System.out.println(message);
+            }
+            userTypeCB.valueProperty().setValue(null);
+            itemSelectionCB.valueProperty().setValue(null);
+            userNameTF.clear();
+        });
+
+        userViewBTN.setOnAction(ActionEvent->{
+            ResultSet resultSet=null;
+            try {
+                Connection conn = DriverManager.getConnection(DB_URL);
+                PreparedStatement stmt = conn.prepareStatement("SELECT * from users");
+                resultSet = stmt.executeQuery();
+                usersListView.getItems().clear();
+                while (resultSet.next()){
+                    String userType = resultSet.getString("User_Type");
+                    String userName = resultSet.getString("User_Name");
+                    String item = resultSet.getString("Item");
+                    usersList.addAll(userType + "     " + userName + "     " + item);
                 }
                 resultSet.close();
                 conn.close();
